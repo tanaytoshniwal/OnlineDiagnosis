3e6de8f026ee5513bbdf467f753ac02a1c21ebef
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-19 11:31:35 +0530

    medical

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import Info from './components/Info/Info';
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      gender: '',
+      age: -1,
+      
+    }
+  }
   render(){
     return (
       <div>
