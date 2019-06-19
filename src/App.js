c240e8595447c8bf5ffab163f9b0cb335af09c24
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-19 11:55:18 +0530

    api key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import Info from './components/Info/Info';
 class App extends React.Component {
+  url = 'https://api.infermedica.com/v2/';
+  headers = {
+    "Content-Type": "application/json",
+    "App-Key": "e8f81759888e6e92cb78e9beccb333ad",
+    "App-Id":"0bdf6dc0"
+  };
   constructor(){
     super()
     this.state = {
