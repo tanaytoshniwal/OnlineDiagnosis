5f72dbca3ed35f1bab4e17cdffe71ec797b5aa82
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-21 11:52:45 +0530

    modal ui with data

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, Alert, Badge} from 'react-bootstrap'
 import './Popup.css'
 function Popup(props) {
     let buttons = props.data.question.items[0].choices.map((data, index)=>
-        <Button variant="outline-info" onClick={()=>{props.click(data.id, props.data.question.items[0].id)}} key={data.id} className="mx-3 choice">
+        <Button variant="outline-info" onClick={()=>{props.click(data.id, props.data.question.items[0].id)}} key={data.id} className="mx-3 my-2 choice">
             {data.label}
         </Button>
+    )
+    let conditions = props.data.conditions.map((data, index)=>
+        <Badge className="m-1" variant="secondary" key={data.id}>{data.common_name} | {parseFloat(data.probability*100).toFixed(1)}%</Badge>
     )
     return (
         <div className="container">
             <div className="row justify-content-end">
                 <button className="btn btn-outline-danger ml-auto" onClick={props.closeModal}>X</button>
             </div>
+            <Alert variant='danger' className="row h5 mt-2">
+                <div className="h4">You are diagnosed with:</div>
+                {conditions}
+            </Alert>
             <div className="row h2">
                 {props.data.question.text}
             </div>
